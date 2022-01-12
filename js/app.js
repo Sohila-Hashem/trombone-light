6ec85a7b944d1fc7bff7f9f8c874c81a968e7e1f
sohila samir <sohila.samir1911@gmail.com>
2022-01-12 15:19:37 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+//.............global variables.........
+//.............STRAT....................
+let burger = document.querySelector('#burger');
+let secMenuItems = document.querySelectorAll('.menu-item');
+let nav = document.getElementsByTagName('nav');
+let submitBtn  = document.querySelector('#submit-btn');
+//..............END...................
+
+//toggle menulist on small screens
+burger.addEventListener('click', (e) => {
+    e.preventDefault()
+    secMenuItems.forEach((menuItem) => {
+        menuItem.classList.toggle('menu-item')
+    })
+})
+
+//turning off the submit action for now
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+})
