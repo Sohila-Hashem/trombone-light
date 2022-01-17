f0d18fcc0713cbb786c1910d4babc492ef39f73c
sohila samir <sohila.samir1911@gmail.com>
2022-01-17 18:23:50 +0200

    new style

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@
 let burger = document.querySelector('#burger');
 let secMenuItems = document.querySelectorAll('.menu-item');
 let nav = document.getElementsByTagName('nav');
-let submitBtn  = document.querySelector('#submit-btn');
 //..............END...................
 
 //toggle menulist on small screens
