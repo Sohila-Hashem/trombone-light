9cfc19e4aa84eb0b28d886458798ed89c2470da5
sohila samir <sohila.samir1911@gmail.com>
2022-06-16 16:21:14 +0200

    fixing minor bugs in the navbar & decreasing font size on some elements

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
-const burger = document.querySelector('#burger');
-const navBtnsContainer = document.getElementById('nav-btn-container');
-const barOne = document.querySelector('.bar-1')
-const barTwo = document.querySelector('.bar-2')
-const barThree = document.querySelector('.bar-3')
-const navbar = document.querySelector('#navbar-container')
+const burger = document.querySelector('.burger');
+const navLinksContainer = document.querySelector('.nav-links-container');
+const linksContainer = document.querySelector('.links-container') ;
+const barOne = document.querySelector('.bar-1');
+const barTwo = document.querySelector('.bar-2');
+const barThree = document.querySelector('.bar-3');
+const navbar = document.querySelector('.navbar-container');
+
 
 document.addEventListener('scroll', (e) => {
-    if (window.scrollY > 168) {
-        navbar.style.backgroundColor = 'rgba(0 , 0 , 0, 0.7)'
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = 'rgb(0,0,0)'
     } else {
         navbar.style.backgroundColor = ''
     }
@@ -17,13 +19,11 @@ burger.addEventListener('click', (e) => {
     barTwo.classList.toggle('bar-2-animate')
     barOne.classList.toggle('bar-1-animate')
     barThree.classList.toggle('bar-3-animate')
-    if (navBtnsContainer.className === 'nav-btn-container-show') {
-        navBtnsContainer.classList.remove('nav-btn-container-show')
-        navBtnsContainer.classList.add('nav-btn-container-hide')
-    } else if (navBtnsContainer.className === 'nav-btn-container-hide') {
-        navBtnsContainer.classList.remove('nav-btn-container-hide')
-        navBtnsContainer.classList.add('nav-btn-container-show')
+    let navLinksContainerHeight = navLinksContainer.getBoundingClientRect().height
+    let linksContainerHeight = linksContainer.getBoundingClientRect().height
+    if (navLinksContainerHeight === 0) {
+        navLinksContainer.style.height = `${linksContainerHeight}px`
     } else {
-        navBtnsContainer.classList.add('nav-btn-container-show')
+        navLinksContainer.style.height = '0px'
     }
 })
